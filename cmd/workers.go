639e1c0706bf8313c390d7970b1e820c44a9f893
Dmitrii Khramtsov <dmitrypkh@gmail.com>
2025-04-25 16:55:11 +0300

    worker pool implementation

---
+++ cmd/workers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Job struct {
+	page    Page
+	pageNum int
+}
+
+type JobErr struct {
+	err      error
+	workerID int
+}
+
+func worker(id int, jobs <-chan Job, errors chan<- JobErr, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	defer func() {
+		if r := recover(); r != nil {
+			errors <- JobErr{err: fmt.Errorf("panic: %v", r), workerID: id}
+		}
+	}()
+
+	for job := range jobs {
+		err := job.page.extract(job.pageNum)
+		if err != nil {
+			errors <- JobErr{err: err, workerID: id}
+		}
+	}
+}
