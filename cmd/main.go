35223baf0d8b606f837fbaac059ad3c34d458891
Dmitrii Khramtsov <dmitrypkh@gmail.com>
2025-04-29 12:44:45 +0300

    move setting to config struct

--- cmd/main.go
+++ cmd/main.go
@@ -21,109 +21,136 @@ const (
 	thumbScaleDownDefault = 10.0
 )
 
+type config struct {
+	sourcePath string
+	saveDir    string
+	prefix     string
+	postfix    string
+	pages      string
+	image      struct {
+		imgSize      string
+		imgScaleDown float64
+		imgType      string
+	}
+	thumb struct {
+		createThumbnails bool
+		thumbScaleDown   float64
+		thumbnailsSize   string
+	}
+	workersNum  int
+	force       bool
+	versionFlag bool
+	quiet       bool
+	debug       bool
+}
+
 func main() {
-	var sourcePath, saveDir, imgType, imgSize, pages, thumbnailsSize, prefix, postfix string
-	var imgScaleDown, thumbScaleDown float64
-	var createThumbnails, force, versionFlag bool
-	var workersNum int
 	var sizeX, sizeY, thumbSizeX, thumbSizeY int
 	var err error
+	var quiet, anyErr bool
+
+	var cfg config
 
 	workersNumDefault := runtime.NumCPU()
 
 	// ImgFormatValidator ImgSizeValidator
-	pflag.StringVarP(&sourcePath, "source", "s", "test.pdf",
+	pflag.StringVarP(&cfg.sourcePath, "source", "s", "test.pdf",
 		"Specify path to source file (pdf/pptx)")
-	pflag.StringVarP(&saveDir, "output", "o", "", "Specify output folder path")
+	pflag.StringVarP(&cfg.saveDir, "output", "o", "", "Specify output folder path")
 
 	// FilenameValidator
-	pflag.StringVarP(&prefix, "prefix", "p", "page", "Prefix for a filename")
-	pflag.StringVarP(&postfix, "postfix", "x", "", "Postfix for a filename")
+	pflag.StringVarP(&cfg.prefix, "prefix", "p", "page", "Prefix for a filename")
+	pflag.StringVarP(&cfg.postfix, "postfix", "x", "", "Postfix for a filename")
 
-	pflag.StringVarP(&imgSize, "size", "S", "",
+	pflag.StringVarP(&cfg.image.imgSize, "size", "S", "",
 		"Specify image size, example 640x480, if not specified will output default size from document")
-	pflag.Float64VarP(&imgScaleDown, "scale", "C", imgScaleDownDefault,
+	pflag.Float64VarP(&cfg.image.imgScaleDown, "scale", "C", imgScaleDownDefault,
 		"Specify image scaling down factor, example 5, for example 5 means output image will be 5 times smaller than original image")
-	pflag.StringVarP(&imgType, "format", "F", "png",
+	pflag.StringVarP(&cfg.image.imgType, "format", "F", "png",
 		"Specify output image format (png/jpg)")
 
-	pflag.StringVarP(&pages, "pages", "P", "",
+	pflag.StringVarP(&cfg.pages, "pages", "P", "",
 		"Use this flag to extract specific pages, example: 2,3,6-8,10")
 
-	pflag.BoolVarP(&createThumbnails, "thumb", "t", false, "enable thumbnails generation")
-	pflag.Float64VarP(&thumbScaleDown, "tscale", "c", thumbScaleDownDefault,
+	pflag.BoolVarP(&cfg.thumb.createThumbnails, "thumb", "t", false, "enable thumbnails generation")
+	pflag.Float64VarP(&cfg.thumb.thumbScaleDown, "tscale", "c", thumbScaleDownDefault,
 		"Specify thumbnails scaling down factor, for example 5 means thumbnail will be 5 times smaller than original image")
-	pflag.StringVarP(&thumbnailsSize, "tsize", "z", "",
+	pflag.StringVarP(&cfg.thumb.thumbnailsSize, "tsize", "z", "",
 		"Specify thumbnails size e.g. 64x64")
 
-	pflag.BoolVarP(&force, "force", "f", false,
+	pflag.BoolVarP(&cfg.force, "force", "f", false,
 		"Don't ask for rewriting is directory contains files")
-	pflag.BoolVar(&versionFlag, "version", false, "Show version")
+	pflag.BoolVarP(&cfg.versionFlag, "version", "v", false, "Show version")
 
-	pflag.IntVarP(&workersNum, "workers", "w", workersNumDefault,
+	pflag.IntVarP(&cfg.workersNum, "workers", "w", workersNumDefault,
 		"Set number of anynchronous workers")
+
+	pflag.BoolVarP(&cfg.quiet, "quiet", "q", false, "Quiet mode")
 
 	pflag.Parse()
 
-	if imgSize != "" && imgScaleDown != imgScaleDownDefault {
-		log.Println(fmt.Sprintf("Choose either scaling factor (--scale) or exact image size for resizing (--size)"))
-		return
+	if cfg.image.imgSize != "" && cfg.image.imgScaleDown != imgScaleDownDefault {
+		fmt.Fprintf(os.Stderr, "Choose either scaling factor (--scale) or exact image size for resizing (--size)")
+		anyErr = true
+	}
+	if err = input.ImgFormatValidator(cfg.image.imgType); err != nil {
+		fmt.Fprintf(os.Stderr, "Unsupported image type: %s", cfg.image.imgType)
+		anyErr = true
+	}
+	if cfg.thumb.thumbnailsSize != "" && cfg.thumb.thumbScaleDown != thumbScaleDownDefault {
+		fmt.Fprintf(os.Stderr, "Choose either scaling factor (--scale) or exact image size for resizing (--size)")
+		anyErr = true
 	}
 
-	if err := input.ImgFormatValidator(imgType); err != nil {
-		log.Println(fmt.Sprintf("Unsupported image type: %s", imgType))
-		return
-	}
-	if thumbnailsSize != "" && thumbScaleDown != thumbScaleDownDefault {
-		log.Println(fmt.Sprintf("Choose either scaling factor (--scale) or exact image size for resizing (--size)"))
-		return
-	}
-
-	if prefix != "" {
-		if err = input.FilenameValidator(prefix); err != nil {
-			log.Println(fmt.Sprintf("Invalid prefix: %s. Error: %s", prefix, err))
+	if cfg.prefix != "" {
+		if err = input.FilenameValidator(cfg.prefix); err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid prefix: %s. Error: %s", cfg.prefix, err)
+			anyErr = true
 		}
 	}
-	if postfix != "" {
-		if err = input.FilenameValidator(postfix); err != nil {
-			log.Println(fmt.Sprintf("Invalid postfix: %s. Error: %s", postfix, err))
+	if cfg.postfix != "" {
+		if err = input.FilenameValidator(cfg.postfix); err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid postfix: %s. Error: %s", cfg.postfix, err)
+			anyErr = true
 		}
 	}
 
-	if workersNum <= 0 {
-		log.Println("Number of workers must be at least 1")
-		return
-	} else {
-		log.Println("Number of workers is", workersNum)
+	if cfg.workersNum <= 0 {
+		fmt.Fprintf(os.Stderr, "Number of workers must be at least 1")
+		anyErr = true
 	}
 
-	log.Println(fmt.Sprintf("Setting image format to %s, save folder: %s", imgType, saveDir))
-	if pages != "" {
-		log.Println(fmt.Sprintf("Selected pages will be extracted: %s", pages))
+	fmt.Printf("\nSetting image format to %s, save folder: %s\n", cfg.image.imgType, cfg.saveDir)
+	if cfg.pages != "" {
+		fmt.Printf("Selected pages will be extracted: %s\n", cfg.pages)
 	}
-	if imgSize != "" {
-		sizeX, sizeY, err = input.ImgSizeExtractor(imgSize)
+	if cfg.image.imgSize != "" {
+		sizeX, sizeY, err = input.ImgSizeExtractor(cfg.image.imgSize)
 		if err != nil {
-			log.Println(fmt.Sprintf("Invalid image size (example: 120x256): %s", err))
-			return
+			fmt.Fprintf(os.Stderr, "Invalid image size (example: 120x256): %s\n", err)
+			anyErr = true
 		}
-		log.Println(fmt.Sprintf("Extracted images size will be set to: %dx%d", sizeX, sizeY))
-	} else if imgScaleDown != 1.0 {
-		log.Println(fmt.Sprintf("Extracted images size will be scaled down with factor %.2f ", imgScaleDown))
+		fmt.Printf("Extracted images size will be set to: %dx%d\n", sizeX, sizeY)
+	} else if cfg.image.imgScaleDown != imgScaleDownDefault {
+		fmt.Printf("Extracted images size will be scaled down with factor %.2f ", cfg.image.imgScaleDown)
 	}
 
-	if thumbnailsSize != "" {
-		thumbSizeX, thumbSizeY, err = input.ImgSizeExtractor(thumbnailsSize)
+	if cfg.thumb.thumbnailsSize != "" {
+		thumbSizeX, thumbSizeY, err = input.ImgSizeExtractor(cfg.thumb.thumbnailsSize)
 		if err != nil {
-			log.Println(fmt.Sprintf("Invalid thumbnail size (example: 120x256): %s", err))
-			return
+			fmt.Fprintf(os.Stderr, "Invalid thumbnail size (example: 120x256): %s", err)
+			anyErr = true
 		}
-		log.Println(fmt.Sprintf("Thumbnails size will be set to: %dx%d", thumbSizeX, thumbSizeY))
-	} else if imgScaleDown != 1.0 {
-		log.Println(fmt.Sprintf("Thumbnails will be resized with scaling down factor %.2f ", imgScaleDown))
+		log.Printf("Thumbnails size will be set to: %dx%d", thumbSizeX, thumbSizeY)
+	} else if cfg.thumb.thumbScaleDown != thumbScaleDownDefault {
+		log.Printf("Thumbnails will be resized with scaling down factor %.2f ", cfg.image.imgScaleDown)
 	}
 
-	if versionFlag {
+	if anyErr {
+		os.Exit(1)
+	}
+
+	if cfg.versionFlag {
 		fmt.Printf("pdfjuicer version %s", version)
 		return
 	}
@@ -133,7 +160,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	doc, err := fitz.New(filepath.Join(workDir, sourcePath))
+	fmt.Println("Start processing...")
+
+	doc, err := fitz.New(filepath.Join(workDir, cfg.sourcePath))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -142,8 +171,8 @@ func main() {
 
 	pageCount := doc.NumPage()
 	var pagesToExtract []int
-	if pages != "" {
-		pagesToExtract, err = input.PagesExtractor(pages, pageCount)
+	if cfg.pages != "" {
+		pagesToExtract, err = input.PagesExtractor(cfg.pages, pageCount)
 		if err != nil {
 			log.Println(err)
 			return
@@ -155,10 +184,10 @@ func main() {
 	}
 
 	var savePath string
-	if createThumbnails {
-		savePath = filepath.Join(workDir, thumbnailsDir, saveDir)
+	if cfg.thumb.createThumbnails {
+		savePath = filepath.Join(workDir, thumbnailsDir, cfg.saveDir)
 	} else {
-		savePath = filepath.Join(workDir, saveDir)
+		savePath = filepath.Join(workDir, cfg.saveDir)
 	}
 	err = os.MkdirAll(savePath, 0755)
 	if err != nil {
@@ -166,17 +195,19 @@ func main() {
 	}
 
 	thumbnails := Thumbnail{
-		isActive:  createThumbnails,
-		scaleDown: thumbScaleDown,
+		isActive:  cfg.thumb.createThumbnails,
+		scaleDown: cfg.thumb.thumbScaleDown,
+		sizeX:     thumbSizeX,
+		sizeY:     thumbSizeY,
 	}
 
 	page := Page{
 		doc:        doc,
-		imgType:    imgType,
+		imgType:    cfg.image.imgType,
 		savePath:   savePath,
-		prefix:     prefix,
-		postfix:    postfix,
-		scaleDown:  imgScaleDown,
+		prefix:     cfg.prefix,
+		postfix:    cfg.postfix,
+		scaleDown:  cfg.image.imgScaleDown,
 		sizeX:      sizeX,
 		sizeY:      sizeY,
 		thumbnails: thumbnails,
@@ -188,25 +219,35 @@ func main() {
 	jobErrors := make(chan JobErr, numJobs)
 	done := make(chan struct{}, numJobs)
 
-	bar := progressbar.Default(int64(numJobs))
+	var bar *progressbar.ProgressBar
+	if !quiet {
+		bar = progressbar.Default(int64(numJobs))
+	}
 
-	for w := 1; w <= workersNum; w++ {
+	for w := 1; w <= cfg.workersNum; w++ {
 		wg.Add(1)
 		go worker(w, jobs, jobErrors, done, &wg)
 	}
 	for _, pageNum := range pagesToExtract {
 		jobs <- Job{page: page, pageNum: pageNum - 1}
 	}
+
 	close(jobs)
 
 	go func() {
 		for i := 0; i < numJobs; i++ {
 			<-done
-			bar.Add(1)
+			if !quiet {
+				bar.Add(1)
+			}
 		}
 	}()
 
 	wg.Wait()
+
+	if !quiet {
+		bar.Finish()
+	}
 
 	close(jobErrors)
 
@@ -214,5 +255,9 @@ func main() {
 		if jobErr.err != nil {
 			log.Printf("Worker %d failed: %v", jobErr.workerID, jobErr.err)
 		}
+	}
+
+	if len(jobErrors) == 0 {
+		fmt.Println("Finished extraction") // here issue happens progressbar printed second time
 	}
 }
